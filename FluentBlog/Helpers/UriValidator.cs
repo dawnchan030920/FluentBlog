2ecfd4649ec47093efdceaf56f8a98aa7c0b8c16
Dawn Chan <dawnchan030920@outlook.com>
2022-07-15 17:30:16 +0800

    新增UriValidator以验证Uri是否匹配。

---
+++ FluentBlog/Helpers/UriValidator.cs
@@ -0,0 +1,29 @@
+﻿using FluentBlog.Model;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Routing;
+
+namespace FluentBlog.Helpers
+{
+    public class UriValidator
+    {
+		private NavigationManager navigationManager;
+
+		public UriValidator(NavigationManager navigationManager)
+        {
+			this.navigationManager = navigationManager;
+        }
+
+        public bool IsActive(MenuItem item)
+        {
+			var href = item?.Url;
+			var match = item?.Match;
+			bool flag = false;
+			if (href is not null)
+			{
+				var relative = $"/{navigationManager.ToBaseRelativePath(navigationManager.Uri).ToLower()}";
+				flag = match == NavLinkMatch.All ? relative == href.ToLower() : relative.StartsWith(href.ToLower());
+			}
+			return flag;
+		}
+    }
+}
