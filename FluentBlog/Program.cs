32963a96bccaf007ef9eff86a8fb68c6ddcaca1c
Dawn Chan <dawnchan030920@outlook.com>
2022-07-18 20:06:44 +0800

    暂时存储进度。

--- FluentBlog/Program.cs
+++ FluentBlog/Program.cs
@@ -9,5 +9,6 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddScoped<MenuService>();
+builder.Services.AddScoped<UriValidator>();
 
 await builder.Build().RunAsync();
