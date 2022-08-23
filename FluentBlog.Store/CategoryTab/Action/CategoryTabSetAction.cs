37f5a5dffe40805942e466404248c746d3939dcf
Dawn Chan <dawnchan030920@outlook.com>
2022-08-24 01:47:08 +0800

    Add forced tab determination of Category page.

---
+++ FluentBlog.Store/CategoryTab/Action/CategoryTabSetAction.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FluentBlog.Store.CategoryTab.Action
+{
+	public class CategoryTabSetAction
+	{
+		public string TabActiveId { get; set; }
+
+		public CategoryTabSetAction(string tabActiveId)
+		{
+			TabActiveId = tabActiveId;
+		}
+	}
+}
---
+++ FluentBlog.Store/CategoryTab/CategoryTabFeature.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentBlog.Service.Category;
+using Fluxor;
+
+namespace FluentBlog.Store.CategoryTab
+{
+	public class CategoryTabFeature : Feature<CategoryTabState>
+	{
+		public override string GetName() => "CategoryTab";
+
+		protected override CategoryTabState GetInitialState()
+		{
+			return new CategoryTabState
+			{
+				TabActiveId = CategoryTabProvider.CategoryPanelId,
+				ByForce = false
+			};
+		}
+	}
+}
