342118cedc93b54df662f92658a1da25e18ac633
Dawn Chan <dawnchan030920@outlook.com>
2022-08-23 15:30:20 +0800

    1. Add tab to Category page. 2. Bug fixes.

---
+++ FluentBlog.Service/Category/CategoryTabProvider.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FluentBlog.Service.Category
+{
+	public static class CategoryTabProvider
+	{
+		public static readonly string ArticlePanelId = "article";
+
+		public static readonly string CategoryPanelId = "category";
+	}
+}
